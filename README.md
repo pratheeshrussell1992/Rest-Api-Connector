186ac9b8ac2797c2e97405a1a712cd82b922b44f
pratheeshrussell1992 <pratheeshrussell@agilexcyber.in>
2022-03-29 16:07:41 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rest Api Connector For Power Bi
+
+## Add the extension
+* Download the REST API CONNECTOR.mez  
+* paste it in C:\Users\<USER>\Documents\Power BI Desktop\Custom Connectors   
+* You may need to 'Allow loading of all extensions' in security settings  
+
+## File of interest
+The function to fetch the data from api is written in file REST_API_CONNECTOR.pq
+
+## Versions
+Visual Studio 2019
---
+++ obj/Release/REST_API_CONNECTOR.m
@@ -0,0 +1,50 @@
+﻿// This file contains your Data Connector logic
+section REST_API_CONNECTOR;
+
+[DataSource.Kind="REST_API_CONNECTOR", Publish="REST_API_CONNECTOR.Publish"]
+shared REST_API_CONNECTOR.Contents = (url as text) =>
+    let
+        currentCredentials = Extension.CurrentCredential(),
+        authenticationKind = currentCredentials[AuthenticationKind],
+        access_token = currentCredentials[Key],
+
+        headers = if authenticationKind = "Key" then
+                    [#"Authorization" = "Bearer " & access_token, #"Content-Type" = "application/json"]
+                else
+                    [#"Content-Type" = "application/json"],
+        
+        Survey = Web.Contents(url, [ Headers = headers, ManualCredentials = true] ),
+        Source = Json.Document(Survey)
+    in
+        Source;
+
+
+// Data Source Kind description
+REST_API_CONNECTOR = [
+    Authentication = [
+        // Key = [],
+        // UsernamePassword = [],
+        // Windows = [],
+        Implicit = [],
+        Key = [
+            KeyLabel = "Auth Key",
+            Label = "Bearer Token"
+        ]
+    ],
+    Label = Extension.LoadString("DataSourceLabel")
+];
+
+// Data Source UI publishing description
+REST_API_CONNECTOR.Publish = [
+    Beta = false,
+    Category = "Other",
+    ButtonText = { Extension.LoadString("ButtonTitle"), Extension.LoadString("ButtonHelp") },
+    LearnMoreUrl = "https://powerbi.microsoft.com/",
+    SourceImage = REST_API_CONNECTOR.Icons,
+    SourceTypeImage = REST_API_CONNECTOR.Icons
+];
+
+REST_API_CONNECTOR.Icons = [
+    Icon16 = { Extension.Contents("REST_API_CONNECTOR16.png"), Extension.Contents("REST_API_CONNECTOR20.png"), Extension.Contents("REST_API_CONNECTOR24.png"), Extension.Contents("REST_API_CONNECTOR32.png") },
+    Icon32 = { Extension.Contents("REST_API_CONNECTOR32.png"), Extension.Contents("REST_API_CONNECTOR40.png"), Extension.Contents("REST_API_CONNECTOR48.png"), Extension.Contents("REST_API_CONNECTOR64.png") }
+];
